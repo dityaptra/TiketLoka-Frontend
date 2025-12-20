05788ab05d3c44f669cf0547ea33c916a5160f47
Ditya <gdevelop351@gmail.com>
2025-12-20 21:09:19 +0800

    remove fb

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -29,14 +29,6 @@ const GoogleIcon = () => (
   </svg>
 );
 
-const FacebookIcon = () => (
-  <svg className="w-5 h-5" viewBox="0 0 24 24">
-    <path
-      d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
-      fill="#1877F2"
-    />
-  </svg>
-);
 
 // --- BAGIAN 1: ISI KONTEN ---
 function RegisterContent() {
@@ -103,9 +95,6 @@ function RegisterContent() {
 
   const handleGoogleLogin = () => {
     window.location.href = `${BASE_URL}/auth/google`;
-  };
-  const handleFacebookLogin = () => {
-    window.location.href = `${BASE_URL}/auth/facebook`;
   };
 
   const inputWrapperClass = "relative flex items-center";
@@ -284,22 +273,15 @@ function RegisterContent() {
             </div>
 
             {/* Social Login Buttons */}
-              <div className="mt-1 grid grid-cols-2 gap-3">
-                <button
-                  type="button"
-                  onClick={handleGoogleLogin}
-                  className="flex items-center justify-center cursor-pointer gap-2 bg-white border border-gray-200 hover:bg-gray-50 text-gray-700 font-medium py-2 rounded-lg transition-all text-sm shadow-sm hover:shadow"
-                >
-                  <GoogleIcon /> <span className="hidden sm:inline">Google</span>
-                </button>
-                <button
-                  type="button"
-                  onClick={handleFacebookLogin}
-                  className="flex items-center justify-center cursor-pointer gap-2 bg-white border border-gray-200 hover:bg-gray-50 text-gray-700 font-medium py-2 rounded-lg transition-all text-sm shadow-sm hover:shadow"
-                >
-                  <FacebookIcon /> <span className="hidden sm:inline">Facebook</span>
-                </button>
-              </div>
+              <div className="mt-1">
+              <button
+                type="button"
+                onClick={handleGoogleLogin}
+                className="w-full flex items-center justify-center cursor-pointer gap-2 bg-white border border-gray-200 hover:bg-gray-50 text-gray-700 font-medium py-2 rounded-lg transition-all text-sm shadow-sm hover:shadow"
+              >
+                <GoogleIcon /> <span>Masuk dengan Google</span>
+              </button>
+            </div>
 
             <div className="text-center text-[10px] md:text-xs text-gray-500 leading-relaxed px-2 border-t mt-1 pt-4 border-gray-100">
               Dengan mendaftar, Anda menyetujui{" "}
