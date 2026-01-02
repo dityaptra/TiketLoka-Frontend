c2498d6a60d48fbc3caed64fcb3fbf79f3dc9553
Ditya <gdevelop351@gmail.com>
2026-01-02 19:31:26 +0800

    mengembalikan tikets

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -51,7 +51,6 @@ export default function MyTicketsPage() {
       try {
         const res = await fetch(`${BASE_URL}/api/my-bookings`, {
           method: "GET",
-          credentials: "include", 
           headers: {
             "Content-Type": "application/json",
             Accept: "application/json",
